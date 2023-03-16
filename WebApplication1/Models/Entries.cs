c753aa1310d6f92393a1b3568ae46a350c8305a7
ALI HAIDER <ALI HAIDER@DESKTOP-8J1L2EH>
2023-03-16 21:20:54 +0500

    CRUD for Entries in local database has been added

--- WebApplication1/Models/Entries.cs
+++ WebApplication1/Models/Entries.cs
@@ -2,15 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+
 
 namespace WebApplication1.Models
 {
-    public class entries
+    public class Entries
     {
         public int ID { get; set; }
         public string Title { get; set; }
         public DateTime ReleaseDate { get; set; }
         public string Genre { get; set; }
         public decimal Price { get; set; }
+    }
+    public class EntriesDBContext: DbContext 
+    {
+        public DbSet<Entries> entries { set; get; }    
     }
 }
