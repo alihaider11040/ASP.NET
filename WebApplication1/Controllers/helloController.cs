45d0b51e91ade2d2d054a6a2d3345769cd6e0195
ALI HAIDER <ALI HAIDER@DESKTOP-8J1L2EH>
2023-03-16 20:09:41 +0500

    Add project files.

---
+++ WebApplication1/Controllers/helloController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class helloController : Controller
+    {
+        // GET: hello
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Welcome(string name , int intNums)
+        {
+            ViewBag.Message = "Hello " + name;
+            ViewBag.NumTimes = intNums;
+
+            return View();
+        }
+    }
+}
