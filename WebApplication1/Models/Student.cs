4fe4b27c2c8f628a864dd9ff0877cc75b5fda314
ALI HAIDER <ALI HAIDER@DESKTOP-8J1L2EH>
2023-03-21 00:00:31 +0500

    Student entered

---
+++ WebApplication1/Models/Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+
+namespace WebApplication1.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        public string name { get; set; }
+        public DateTime DOB { get; set; }
+
+    }
+    public class StudentDBContext : DbContext 
+    {
+    public DbSet<Student> Students { set; get; }
+    }
+}
